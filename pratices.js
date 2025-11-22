ce034750118e965d0a0fb08b5dab4132fb02a78d
dhanushp1805 <dhanushtetro1805@gmail.com>
2025-11-22 10:15:31 +0530

    jss

--- pratices.js
+++ pratices.js
@@ -16,7 +16,7 @@
 
 // console.log("Selected color " + currentColor );
 
-// Switch-case
+////////////////////////////////////////// Switch-case //////////////////////////////////////////
 // Ex: Grade
 // let grade = "E";
 
@@ -55,14 +55,14 @@
 //     console.log("Under amount");
 // }
 
-// for Loop
+////////////////////////////////////////// for Loop //////////////////////////////////////////
 
 // for (let i = 0; i <= 10; i++) {
 //   let print = i % 2 !== 0 ? `odd Number ${i}` : `Even number ${i}`;
 //   console.log(print);
 // }
 
-// While Loop
+////////////////////////////////////////// While Loop //////////////////////////////////////////
 // let i = 0;
 
 // while (i <= 10) {
@@ -74,7 +74,7 @@
 //   i++;
 // }
 
-// DO-While loop
+////////////////////////////////////////// DO-While loop //////////////////////////////////////////
 // let i = 0;
 // do {
 //   if (i % 2 === 0) {
@@ -85,8 +85,8 @@
 //   i++;
 // } while (i <= 10);
 
-// for in loop
-// Object for-n loop
+////////////////////////////////////////// for in loop //////////////////////////////////////////
+// Object for-in loop
 // let person = {
 //   name: "Sri",
 //   age: 17,
@@ -104,23 +104,22 @@
 // //   console.log(`${key} : ${colors[key]}`);
 // // }
 
-// // for of loop
+////////////////////////////////////////// for of loop //////////////////////////////////////////
 // for (let index of colors) {
 //   // index is color ooda index value and of is color ooda value yeduthukkum
 //   console.log(`color : ${index}`);
 // }
 
+////////////////////////////////////////// All function //////////////////////////////////////////
 
-// All function
-
-const person1 = {
-    name : "Sri",
-    age : 17,
-}
-const person2 = {
-    name : "Dav",
-    age : 17,
-}
+// const person1 = {
+//   name: "Sri",
+//   age: 17,
+// };
+// const person2 = {
+//   name: "Dav",
+//   age: 17,
+// };
 
 // function personName(){
 //     console.log(person.name)
@@ -128,16 +127,15 @@ const person2 = {
 
 // personName()
 // parameter and Argument function
-function personName(name){ /// name is parameter
-    console.log(name);
-    console.log(age)
-}
+// function personName(name){ /// name is parameter
+//     console.log(name);
+//     console.log(age)
+// }
 
-personName(person1.name, ) /// person.name is argument
-personName(person2.name) /// person.name is argument
+// personName(person1.name, ) /// person.name is argument
+// personName(person2.name) /// person.name is argument
 
-
-// function return
+////////////////////////////////////////// function return //////////////////////////////////////////
 
 // function fullName (firstName, lastName){
 //     return firstName + lastName
@@ -147,7 +145,7 @@ personName(person2.name) /// person.name is argument
 
 // console.log(personName)
 
-// anonymous function 
+////////////////////////////////////////// anonymous function //////////////////////////////////////////
 // let fullName = function (firstName, lastName) {
 //     return firstName + " " + lastName
 // }
@@ -155,7 +153,7 @@ personName(person2.name) /// person.name is argument
 // let personName = fullName("Lavanya", "sri");
 // console.log(personName)
 
-// Arrow function 
+////////////////////////////////////////// Arrow function //////////////////////////////////////////
 // let fullName =  (firstName, lastName) =>{
 //     return firstName + " " + lastName
 // }
@@ -163,12 +161,107 @@ personName(person2.name) /// person.name is argument
 // let personName = fullName("Lavanya", "sri");
 // console.log(personName)
 
-// Array Methods
+////////////////////////////////////////// Array Methods //////////////////////////////////////////
 
-// foreach method 
+// foreach method ////////////
+// let fruits = ["Apple", "banana", "orange"];
 
-let fruits = ["Apple", "banana", "orange"];
+// fruits.forEach((index) => {
+// console.log(index.toUpperCase())
+// });
 
-fruits.forEach((index) => {
-console.log(index.toUpperCase())
-});
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr1 = [
+//   {
+//     id: 0,
+//     name: "Sri",
+//   },
+//   {
+//     id: 1,
+//     name: "Dev",
+//   },
+//   {
+//     id: 2,
+//     name: "Ram",
+//   },
+//   {
+//     id: 3,
+//     name: "Ravi",
+//   },
+// ];
+// arr.forEach((element) => {
+//   console.log(element);
+// });
+// arr1.forEach((element1, index, arr) => {
+//   if (index === 0) {
+//     console.log(element1.id + " " + element1.name);
+//   } else if (index === 1) {
+//     console.log(element1.id + " " + element1.name);
+//   }
+// });
+
+// arr1.forEach((user, index) => {
+//   console.log(`${index}: ${user.id} ${user.name}`);
+// });
+
+// let totalId = arr1.reduce((total, item) => {
+//   return total + item.id;
+// }, 0);
+// console.log(totalId);
+
+// let num = [1, 2, 3, 4, 5, 7, 8, 9, 10];
+
+// function rest(...arg) {
+//   for (let key of arg) {
+//     console.log(`${key}`);
+//   }
+// }
+
+// rest(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+////////////////////////////////////////// Object //////////////////////////////////////////
+
+let person = {
+  firstName: "lavanya",
+  lastName: "sri",
+  age: 17,
+  gender: "female",
+  isAlive: true,
+  parent: {
+    father: "Vijay",
+    mother: "Thirsha",
+    brother: "Dhanush",
+  },
+  fullName: function () {
+    return this.firstName + this.lastName;
+  },
+};
+
+// let personMethods = {
+//   yearofBirth() {},
+// };
+
+// console.log(person.fullName());
+// console.log(person);
+
+// Object Merging
+// Object.assign(person, personMethods);
+// console.log(person);
+
+// Spread Operator
+// let finlObj = { ...person, ...personMethods };
+// console.log(finlObj);
+
+// // delete
+// delete person.age;
+// console.log(person);
+
+////////////////////////////////////////// JavaScript Object Destructuring //////////////////////////////////////////
+
+// let { firstName: myfirstName = "dhanu", lastName, age = 20, favColor="Green" } = person;
+// console.log(myfirstName, lastName, age, favColor);
+
+let {firstName, lastName, ...remainingItems} = person;
+
+console.log(firstName, lastName, remainingItems);
+
